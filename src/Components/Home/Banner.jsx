d63dce59738606a2a9d134c66094dd2c484d9a9d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-10-07 21:37:28 +0600

    banner section added

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -2,8 +2,17 @@
 
 const Banner = () => {
     return (
-        <div>
-          <h2>This is Banner section</h2>  
+        <div className="mt-48">
+            <div className="hero w-full h-[750px] mt-16" style={{ backgroundImage: 'url(https://i.ibb.co/vHSKttB/Screenshot-973.png)' }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="">
+                        <h1 className="mb-5 text-6xl font-bold text-center">Welcome to Medicio </h1>
+                        <p className="mb-5 text-xl">Our hospital mission is to providing high-quality healthcare services. Our  aims to establish trust and convey the hospital dedication to patient-centered care and community well-being.</p>
+                        <button className="btn btn-primary bg-green-600 border-none hover:bg-green-700">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
